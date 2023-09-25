306da80aac2132e14bd674134e06f0c8b39ec958
Guillaume Falourd <guillaume.falourd@zup.com.br>
2023-09-25 14:06:46 -0300

    first commit
    Signed-off-by: Guillaume Falourd <guillaume.falourd@zup.com.br>

---
+++ runtime.py
@@ -0,0 +1,125 @@
+import os
+import requests
+import yaml
+import json
+from pathlib import Path
+
+ACTION_PATH = os.getenv("ACTION_PATH")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_KEY = os.getenv("CLIENT_KEY")
+CLIENT_REALM = os.getenv("CLIENT_REALM")
+VERSION_TAG = os.getenv("VERSION_TAG")
+CHECKOUT_BRANCH = os.getenv("CHECKOUT_BRANCH")
+TF_STATE_BUCKET_NAME = os.getenv("TF_STATE_BUCKET_NAME")
+TF_STATE_REGION = os.getenv("TF_STATE_REGION")
+IAC_BUCKET_NAME = os.getenv("IAC_BUCKET_NAME")
+IAC_REGION = os.getenv("IAC_REGION")
+
+inputs_list = [ACTION_PATH, CLIENT_ID, CLIENT_KEY, CLIENT_REALM, VERSION_TAG, TF_STATE_BUCKET_NAME, TF_STATE_REGION, IAC_BUCKET_NAME, IAC_REGION]
+
+if None in inputs_list:
+    print("Some input is empty")
+    exit()
+
+if CHECKOUT_BRANCH is None:
+    CHECKOUT_BRANCH = 'main'
+
+manifesto = yaml.safe_load(Path(ACTION_PATH+"/manifest-app.yaml").read_text())
+print("MANIFESTO", manifesto)
+
+manifestoType = manifesto["kind"]
+if manifestoType == 'application':
+    appOrInfraId=manifesto["kind"]["spec"]["appId"]
+if manifestoType == 'shared_infrastructure':
+    appOrInfraId=manifesto["kind"]["spec"]["infraId"]
+print(f"{manifestoType} project identified, with ID: {appOrInfraId}")
+
+idm_url = f"https://idm.stackspot.com/realms/{CLIENT_REALM}/protocol/openid-connect/token"
+idm_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+idem_params = { "client_id":f"{CLIENT_ID}", "grant_type":"client_credentials", "client_secret":f"{CLIENT_KEY}" }
+
+r1 = requests.post(
+        url=idm_url, 
+        headers=idm_headers, 
+        params=idem_params
+    )
+
+if r1.status_code == 200:
+    d1 = r1.json()
+    access_token = d1["access_token"]
+    
+    with open('config.json', 'w') as json_file:
+        json.dump(manifesto, json_file)
+    
+    manifesto_json = json.dumps(json.load(open('config.json')), indent=2)
+    envId = manifesto["spec"]["appliedPlugins"][0]["inputs"]["stk_env_id"]
+    wksId = manifesto["spec"]["appliedPlugins"][0]["inputs"]["stk_wks_id"]
+
+    request_data = {
+        "config": {
+            "tfstate": {
+              "bucket": TF_STATE_BUCKET_NAME,
+              "region": TF_STATE_REGION
+        },
+        "iac": {
+            "bucket": IAC_BUCKET_NAME,
+            "region": IAC_REGION
+        },
+        "isApi": False,                      # FOR NOW
+        "apiContractPath": "./swagger.yaml", # FOR NOW
+        "envId": envId,
+        "workspaceId": wksId,
+        "versionTag": VERSION_TAG,
+        "runConfig": {
+            "checkoutBranch": CHECKOUT_BRANCH
+        },
+        "manifesto": manifesto_json
+        }
+    }
+
+    deploy_headers = {"Authorization": f"Bearer {access_token}"}
+
+    if manifestoType == 'application':
+        self_hosted_deploy_app_url="https://runtime-manager.v1.stackspot.com/v1/run/self-hosted/deploy/app"
+        r2 = requests.post(
+                url=self_hosted_deploy_app_url, 
+                headers=deploy_headers
+            )
+    if manifestoType == 'shared_infrastructure':
+        self_hosted_deploy_infra_url="https://runtime-manager.v1.stackspot.com/v1/run/self-hosted/deploy/infra"
+        r2 = requests.post(
+                url=self_hosted_deploy_infra_url, 
+                headers=deploy_headers
+            )
+
+    if r2.status_code == 200:
+        d2 = r2.json()
+        runId = d2["runId"]
+        runType = d2["runType"]
+        tasks = d2["tasks"]
+
+        output_file = os.getenv('GITHUB_OUTPUT')
+        with open(output_file, "a") as myfile:
+            myfile.write(f"runId={runId}")
+            myfile.write(f"runType={runType}")
+            myfile.write(f"tasks={tasks}")
+
+        print(f"Run {runType} successfully started with ID: {runId}")
+
+    else:
+        print("Error starting self hosted deploy")
+        print("Status", r2.status_code)
+        print("Error", r2.reason)
+        exit()
+
+else:
+    print("Error during authentication")
+    print("Status", r1.status_code)
+    print("Error", r1.reason)
+    exit()
+
+
+def convert_manifesto_to_request(manifesto: str):
+    with open('config.json', 'w') as json_file:
+        json.dump(manifesto, json_file)
+    return json.dumps(json.load(open('config.json')), indent=2)

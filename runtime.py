d76474b2803cfc9049864b315769650e35a4d7cc
Rodrigo Cury <rodrigo.baptista@zup.com.br>
2024-03-07 15:37:02 -0300

    Changed serialization strategy

--- runtime.py
+++ runtime.py
@@ -1,12 +1,29 @@
 import os
 import requests
-import yaml
 import json
 from pathlib import Path
+from ruamel.yaml import YAML
+from io import StringIO
 
-def save_output(name: str, value: str): 
+
+def yaml() -> YAML:
+    yml = YAML()
+    yml.indent(mapping=2, sequence=4, offset=2)
+    yml.allow_unicode = True
+    yml.default_flow_style = False
+    yml.preserve_quotes = True
+    return yml
+
+
+def safe_load(content: str) -> dict:
+    yml = yaml()
+    return yml.load(StringIO(content))
+
+
+def save_output(name: str, value: str):
     with open(os.environ['GITHUB_OUTPUT'], 'a') as output_file:
         print(f'{name}={value}', file=output_file)
+
 
 ACTION_PATH = os.getenv("ACTION_PATH")
 CLIENT_ID = os.getenv("CLIENT_ID")
@@ -18,66 +35,67 @@ IAC_BUCKET_NAME = os.getenv("IAC_BUCKET_NAME")
 IAC_REGION = os.getenv("IAC_REGION")
 VERBOSE = os.getenv("VERBOSE")
 
-inputs_list = [ACTION_PATH, CLIENT_ID, CLIENT_KEY, CLIENT_REALM, TF_STATE_BUCKET_NAME, TF_STATE_REGION, IAC_BUCKET_NAME, IAC_REGION]
+inputs_list = [ACTION_PATH, CLIENT_ID, CLIENT_KEY, CLIENT_REALM, TF_STATE_BUCKET_NAME, TF_STATE_REGION, IAC_BUCKET_NAME,
+               IAC_REGION]
 
 if None in inputs_list:
     print("- Some mandatory input is empty. Please, check the input list.")
     exit(1)
 
-with open(Path(ACTION_PATH+'/manifest.yaml'), 'r') as file:
+with open(Path(ACTION_PATH + '/manifest.yaml'), 'r') as file:
     manifesto_yaml = file.read()
 
-manifesto_dict = yaml.safe_load(manifesto_yaml)
+manifesto_dict = safe_load(manifesto_yaml)
 
 if VERBOSE is not None:
     print("- MANIFESTO:", manifesto_dict)
 
 manifestoType = manifesto_dict["manifesto"]["kind"]
-appOrInfraId= manifesto_dict["manifesto"]["spec"]["id"]
+appOrInfraId = manifesto_dict["manifesto"]["spec"]["id"]
 
 print(f"{manifestoType} project identified, with ID: {appOrInfraId}")
 
 idm_url = f"https://account-keycloak.stg.stackspot.com/realms/{CLIENT_REALM}/protocol/openid-connect/token"
 idm_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-idm_data = { "client_id":f"{CLIENT_ID}", "grant_type":"client_credentials", "client_secret":f"{CLIENT_KEY}" }
+idm_data = {"client_id": f"{CLIENT_ID}", "grant_type": "client_credentials", "client_secret": f"{CLIENT_KEY}"}
 
 r1 = requests.post(
-        url=idm_url, 
-        headers=idm_headers, 
-        data=idm_data
-    )
+    url=idm_url,
+    headers=idm_headers,
+    data=idm_data
+)
 
 if r1.status_code == 200:
     d1 = r1.json()
     access_token = d1["access_token"]
-    
+
     version_tag = manifesto_dict["versionTag"]
     if version_tag is None:
         print("- Version Tag not informed or couldn't be extracted.")
-        exit(1) 
-    
+        exit(1)
+
     is_api = manifesto_dict["isApi"]
     if is_api is None:
         print("- API TYPE not informed or couldn't be extracted.")
-        exit(1) 
-    
+        exit(1)
+
     envId = manifesto_dict["envId"]
     if envId is None:
         print("- ENVIRONMENT ID not informed or couldn't be extracted.")
-        exit(1) 
-    
-    wksId = manifesto_dict["workspaceId"] 
+        exit(1)
+
+    wksId = manifesto_dict["workspaceId"]
     if wksId is None:
         print("- WORKSPACE ID not informed or couldn't be extracted.")
-        exit(1) 
+        exit(1)
 
     branch = None
-    if "runConfig" in manifesto_dict: 
+    if "runConfig" in manifesto_dict:
         branch = manifesto_dict["runConfig"]["checkoutBranch"]
         print("Branch informed:", branch)
-    
+
     api_contract_path = None
-    if "apiContractPath" in manifesto_dict: 
+    if "apiContractPath" in manifesto_dict:
         api_contract_path = manifesto_dict["apiContractPath"]
         print("API contract path informed:", api_contract_path)
 
@@ -123,23 +141,27 @@ if r1.status_code == 200:
 
     if VERBOSE is not None:
         print("- DEPLOY RUN REQUEST DATA:", request_data)
-    
+
     deploy_headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
 
     if manifestoType == 'application':
-        self_hosted_deploy_app_url="https://runtime-manager.stg.stackspot.com/v1/run/self-hosted/deploy/app"
+        self_hosted_deploy_app_url = "https://runtime-manager.stg.stackspot.com/v1/run/self-hosted/deploy/app"
         r2 = requests.post(
-                url=self_hosted_deploy_app_url, 
-                headers=deploy_headers,
-                data=request_data
-            )
+            url=self_hosted_deploy_app_url,
+            headers=deploy_headers,
+            data=request_data
+        )
     if manifestoType == 'shared-infrastructure':
-        self_hosted_deploy_infra_url="https://runtime-manager.stg.stackspot.com/v1/run/self-hosted/deploy/infra"
+        self_hosted_deploy_infra_url = "https://runtime-manager.stg.stackspot.com/v1/run/self-hosted/deploy/infra"
         r2 = requests.post(
-                url=self_hosted_deploy_infra_url, 
-                headers=deploy_headers,
-                data=request_data
-            )
+            url=self_hosted_deploy_infra_url,
+            headers=deploy_headers,
+            data=request_data
+        )
+
+    else:
+        print("- MANIFESTO TYPE not informed or couldn't be extracted, should be 'application' or 'shared-infrastructure'")
+        exit(1)
 
     if r2.status_code == 201:
         d2 = r2.json()
